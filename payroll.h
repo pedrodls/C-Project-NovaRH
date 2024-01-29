7a81526ac3581354616d6a9ac2f1c22bbd0f27f7
Bruno Mateus <89973487+Bruno-F-D-Mateus@users.noreply.github.com>
2024-01-29 09:06:31 +0100

    Update payroll.h

--- payroll.h
+++ payroll.h
@@ -3,18 +3,18 @@
 #include "./employee.h"
 #include "./year.h"
 
-// Criar uma folha de pagamento
+// Criar uma folha de salário
 void createPayroll(Employee *employee, QueueYear *year);
-
+//retorna o endereço da próxima folha de salário
 Payroll *getNextPayroll(Payroll *payroll);
-
+//retorna a lista de faltas do funcionário que constarâo na folha
 Absence *getAbsenceFromPayroll(Payroll *payroll);
-
+//descreve os dados da folha de salário
 void describePayroll(Payroll *payroll, Year *year, int type);
-
+//retorna o valor total do bonus
 float getBonusTotalValue(Bonus *bonus, Employee *employee, Payroll *newPayroll);
-
+//retorna o valor total dos descontos
 float getDiscountTotalValue(Absence *absence, Employee *employee, Payroll *newPayroll);
-
+//descreve a folha de salário de acordo com o ano e mês escolhido
 void describeYearHistoryPayroll(Payroll *payroll, int year, Month *month);
 
